0c95f5e1ec6a83dc8ea3fe7ae09cdd29c4418b24
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-12-19 18:38:17 +0100

    fluent page object model for cypress

---
+++ cypress/cypress/e2e/fluent-page-object-model/todo-mvc.cy.ts
@@ -0,0 +1,53 @@
+import TodoPageFluent from '../../fixtures/pages/TodoPageFluent';
+describe('TodoMVC', () => {
+    it('should add new todo', () => {
+        new TodoPageFluent()
+          .visit()
+          .fillNewTodo ('new todo')
+          .submit()
+          .getTodoList()
+          .should('contain', 'new todo');
+    });
+
+    it('should check todo', () => {
+        new TodoPageFluent()
+          .visit()
+          .fillNewTodo ('new todo')
+          .submit()
+          .checkTodo ('new todo')
+          .getTodoElement('new todo')
+          .should('have.class', 'completed');
+    });
+
+    it('should uncheck todo', () => {
+        new TodoPageFluent()
+          .visit()
+          .fillNewTodo ('new todo')
+          .submit()
+          .checkTodo ('new todo')
+          .uncheckTodo ('new todo')
+          .getTodoElement('new todo')
+          .should('not.have.class', 'completed');
+    });
+
+    it('should clear completed todos', () => {
+        new TodoPageFluent()
+          .visit()
+          .fillNewTodo ('new todo')
+          .submit()
+          .checkTodo ('new todo')
+          .clearCompleted()
+          .getTodoList()
+          .should('not.contain', 'new todo');
+    });
+
+    it('should delete todo', () => {
+        new TodoPageFluent()
+          .visit()
+          .fillNewTodo ('new todo')
+          .submit()
+          .deleteTodo ('new todo')
+          .getTodoList()
+          .should('not.contain', 'new todo');
+    });
+});
--- cypress/cypress/fixtures/pages/TodoPageFluent.ts
+++ cypress/cypress/fixtures/pages/TodoPageFluent.ts
@@ -4,23 +4,26 @@ export default class TodoPage {
     /**
      * Visits the specified URL using Cypress.
      */
-    visit() {
+    visit(): TodoPage {
         cy.visit(URL);
+        return this;
     }
 
     /**
      * Fills the new todo input field with the specified text.
      * @param {string} todoText - The text to be typed into the input field.
      */
-    fillNewTodo (todoText: string) {
+    fillNewTodo (todoText: string): TodoPage {
         cy.get('.new-todo').type(todoText);
+        return this;
     }
 
     /**
      * Submits the todo item by pressing the enter key.
      */
-    submit() {
+    submit(): TodoPage {
         cy.get('.new-todo').type('{enter}');
+        return this;
     }
 
     /**
@@ -28,8 +31,9 @@ export default class TodoPage {
      *
      * @param {string} todoText - The text of the todo item to check.
      */
-    checkTodo (todoText: string) {
+    checkTodo (todoText: string): TodoPage {
         cy.contains(todoText).parent().find('.toggle').check();
+        return this;
     }
 
     /**
@@ -37,23 +41,26 @@ export default class TodoPage {
      *
      * @param {string} todoText - The text of the todo item.
      */
-    uncheckTodo (todoText: string) {
+    uncheckTodo (todoText: string): TodoPage {
         cy.contains(todoText).parent().find('.toggle').uncheck();
+        return this;
     }
 
     /**
      * Clears all completed todos.
      */
-    clearCompleted() {
+    clearCompleted(): TodoPage {
         cy.get('.clear-completed').click();
+        return this
     }
 
     /**
      * Deletes a todo item with the specified text.
      * @param {string} todoText - The text of the todo item to be deleted.
      */
-    deleteTodo (todoText: string) {
+    deleteTodo(todoText: string): TodoPage {
         cy.contains(todoText).parent().find('.destroy').click({force: true});
+        return this;
     }
 
     /**
@@ -62,7 +69,7 @@ export default class TodoPage {
      * @param {string} todoText - The text of the todo item.
      * @returns {Cypress.Chainable<Element>} The parent element of the todo item.
      */
-    getTodoElement (todoText: string): Cypress.Chainable<JQuery<HTMLElement>> {
+    getTodoElement(todoText: string): Cypress.Chainable<JQuery<HTMLElement>> {
         return cy.contains(todoText).parent().parent();
     }
 
